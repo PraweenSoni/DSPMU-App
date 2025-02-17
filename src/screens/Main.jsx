0cce97c6465b47f61f48158709a34c138b27a87e
Praween Soni <pramrajkumar1234@gmail.com>
2025-02-17 21:21:21 +0530

    Notice page UI completed

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -10,18 +10,20 @@ function Main() {
     <View>
       <Text> Home page</Text>
       <View>
-        <Text>Dr. Shyama Prasad Mukherjee University</Text>
-        <Text><Text>Add :</Text>P.O. - Ranchi University Morabadi, Ranchi-834008</Text>
-        <Text><Text>Call :</Text>+91 860 304 ****</Text>
-        <Text><Text>Email :</Text>regi****@dspmuranchi.ac.in</Text>
-        <View style={{flexDirection:'row'}}>
+        {/* <FormBox /> */}
+      </View>
+      <View style={styles.contactSec}>
+        <Text style={[styles.contactSecD, {fontWeight: 500}]}>Dr. Shyama Prasad Mukherjee University</Text>
+        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Add : </Text>P.O. - Ranchi University Morabadi, Ranchi-834008</Text>
+        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Call : </Text>+91 860 304 ****</Text>
+        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Email : </Text>regi****@dspmuranchi.ac.in</Text>
+        <View style={styles.contactSecD}>
           <Image style={styles.userImg} source={facebook}/>
           <Image style={styles.userImg} source={instagram}/>
           <Image style={styles.userImg} source={youtube}/>
           <Image style={styles.userImg} source={linkedin}/>
           <Image style={styles.userImg} source={twitter}/>
         </View>
-        <Text>Designed & Build by PKS</Text>
       </View>
     </View>
   )
@@ -30,7 +32,25 @@ function Main() {
 export default Main;
 
 const styles = StyleSheet.create({
+  contactSec: {
+    marginStart: 20
+  },
+  contactSecD: {
+    marginVertical: 1,
+    flexDirection: 'row'
+  },
   userImg: {
-    marginStart: 1
+    marginEnd: 1,
+    height: 28,
+    width: 28,
   },
 })
+
+// function FormBox(num, formName){
+//   return(
+//     <View>
+//       <Text>${num}</Text>
+//       <Text>${formName}</Text>
+//     </View>
+//   )
+// }
