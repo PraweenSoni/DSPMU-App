36ca9f50c20a9cac3c1d386195cee81d549560a9
Praween Soni <pramrajkumar1234@gmail.com>
2025-03-20 15:01:35 +0530

    Feedback page data send to backend & some other changes

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, Modal, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Calendar from '../components/Calendar';
 import Timetable from '../components/TimeTable';
 import NoticePre from '../components/NoticePre';
+import { fetchUserData } from '../services/apiService';
 
 import userImg from '../../assets/user.png';
 import facebook from '../../assets/socialMediaIcons/facebook.png';
@@ -12,8 +13,8 @@ import instagram from '../../assets/socialMediaIcons/instagram.png';
 import youtube from '../../assets/socialMediaIcons/youtube.png';
 import Notices from '../components/Notices';
 
-const FormBox = ({num, formName, color}) => {
-  return(
+const FormBox = ({ num, formName, color }) => {
+  return (
     <View style={styles.formDetail}>
       <Text style={[styles.formNumText, { color: color }]}>{num}</Text>
       <Text>{formName}</Text>
@@ -22,33 +23,67 @@ const FormBox = ({num, formName, color}) => {
 };
 
 function Main() {
+  // Login user name fetch
+  const [studentName, setStudentName] = useState('');
+  useEffect(() => {
+    const loadUserProfile = async () => {
+      const data = await fetchUserData("stuDetails/stuDetails");
+      if (data && data.user) {
+        setStudentName(data.user.name || "N/A");
+      }
+    };
+
+    loadUserProfile();
+  }, []);
   const [selectedNotice, setSelectedNotice] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
 
   const notices = [
-    {title: 'Notice 1', message: 'This is notice 1', date: '23/12'},
-    {title: 'Notice 2', message: 'This is notice 2', date: '24/12'},
-    {title: 'Notice 3', message: 'This is notice 3', date: '25/12'},
-    {title: 'Notice 4', message: 'This is notice 4', date: '26/12'},
-    {title: 'Notice 5', message: 'This is notice 5', date: '27/12'},
+    { title: 'Notice 1', message: 'This is notice 1', date: '23/12' },
+    { title: 'Notice 2', message: 'This is notice 2', date: '24/12' },
+    { title: 'Notice 3', message: 'This is notice 3', date: '25/12' },
+    { title: 'Notice 4', message: 'This is notice 4', date: '26/12' },
+    { title: 'Notice 5', message: 'This is notice 5', date: '27/12' },
   ];
 
   const handleNoticePress = (notice) => {
     setSelectedNotice(notice);
     setModalVisible(true);
   };
+
+  // Code for Student present in class & lab
+  const [tclsrun, setTclsrun] = useState('');
+  const [clspre, setClspre] = useState('');
+  const [tlabrun, setTlabrun] = useState('');
+  const [labpre, setLabpre] = useState('');
+
+  useEffect(() => {
+    const loadStudentPreDetail = async () => {
+      const data = await fetchUserData("stuPresent/stuPresent");
+      if (data) {
+        setTclsrun(data.user.tclsrun);
+        setClspre(data.user.clspre);
+        setTlabrun(data.user.tlabrun);
+        setLabpre(data.user.labpre);
+      }
+    };
+
+    loadStudentPreDetail();
+  }, []);
+
+
   return (
     <ScrollView>
       <View style={styles.userDetail}>
         <View>
           <Text>DSPMU, Ranchi</Text>
-          <Text style={{fontWeight: 500}}>Hi, User</Text>
+          <Text style={{ fontWeight: 500 }}>Hi, {studentName}</Text>
         </View>
-        <Image style={styles.userImg} source={userImg}/>
+        <Image style={styles.userImg} source={userImg} />
       </View>
       <ScrollView nestedScrollEnabled={true} style={styles.noticeSec}>
         {/* <notice Box for all departement /> */}
-        <Text style={{fontSize: 16, marginBottom: 10}}>All Notices</Text>
+        <Text style={{ fontSize: 16, marginBottom: 10 }}>All Notices</Text>
         {notices.map((notice, index) => (
           <Notices
             key={index}
@@ -59,44 +94,44 @@ function Main() {
           />
         ))}
         {selectedNotice && (
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <View style={styles.modalContainer}>
-            <NoticePre
-              title={selectedNotice.title}
-              message={selectedNotice.message}
-              date={selectedNotice.date}
-              onClose={() => setModalVisible(false)}
-            />
-          </View>
-        </Modal>
-      )}
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(false)}
+          >
+            <View style={styles.modalContainer}>
+              <NoticePre
+                title={selectedNotice.title}
+                message={selectedNotice.message}
+                date={selectedNotice.date}
+                onClose={() => setModalVisible(false)}
+              />
+            </View>
+          </Modal>
+        )}
       </ScrollView>
       <View>
         <Timetable />
         <Calendar />
       </View>
       <View style={styles.parentFormBox}>
-        <FormBox num={15} formName="CLASS PRESENT" color="#1ab69d"/>
-        <FormBox num={18} formName="TOTAL CLASS RUN" color="#ee4a62"/>
-        <FormBox num={6} formName="LAB PRESENT" color="#8e56ff"/>
-        <FormBox num={8} formName="TOTAL LAB RUN" color="#f8941f"/>
+        <FormBox num={clspre} formName="CLASS PRESENT" color="#1ab69d" />
+        <FormBox num={tclsrun} formName="TOTAL CLASS RUN" color="#ee4a62" />
+        <FormBox num={labpre} formName="LAB PRESENT" color="#8e56ff" />
+        <FormBox num={tlabrun} formName="TOTAL LAB RUN" color="#f8941f" />
       </View>
       <View style={styles.contactSec}>
-        <Text style={[styles.contactSecD, {fontWeight: 500}]}>Dr. Shyama Prasad Mukherjee University, Ranchi</Text>
-        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Add : </Text>P.O. - Ranchi University Morabadi, Ranchi-834008</Text>
-        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Call : </Text>+91 860 304 ****</Text>
-        <Text style={styles.contactSecD}><Text style={{fontWeight: 500}}>Email : </Text>regi****@dspmuranchi.ac.in</Text>
+        <Text style={[styles.contactSecD, { fontWeight: 500 }]}>Dr. Shyama Prasad Mukherjee University, Ranchi</Text>
+        <Text style={styles.contactSecD}><Text style={{ fontWeight: 500 }}>Add : </Text>P.O. - Ranchi University Morabadi, Ranchi-834008</Text>
+        <Text style={styles.contactSecD}><Text style={{ fontWeight: 500 }}>Call : </Text>+91 860 304 ****</Text>
+        <Text style={styles.contactSecD}><Text style={{ fontWeight: 500 }}>Email : </Text>regi****@dspmuranchi.ac.in</Text>
         <View style={styles.contactSecD}>
-          <Image style={styles.socialMediaImg} source={facebook}/>
-          <Image style={styles.socialMediaImg} source={instagram}/>
-          <Image style={styles.socialMediaImg} source={youtube}/>
-          <Image style={styles.socialMediaImg} source={linkedin}/>
-          <Image style={styles.socialMediaImg} source={twitter}/>
+          <Image style={styles.socialMediaImg} source={facebook} />
+          <Image style={styles.socialMediaImg} source={instagram} />
+          <Image style={styles.socialMediaImg} source={youtube} />
+          <Image style={styles.socialMediaImg} source={linkedin} />
+          <Image style={styles.socialMediaImg} source={twitter} />
         </View>
       </View>
     </ScrollView>
@@ -108,7 +143,7 @@ export default Main;
 const styles = StyleSheet.create({
   userDetail: {
     flexDirection: 'row',
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
     margin: 10
   },
   userImg: {
@@ -129,15 +164,15 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     gap: 10
   },
-  formDetail:{
-    height:80,
+  formDetail: {
+    height: 80,
     width: '45%',
     borderRadius: 7,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#ededed'
   },
-  formNumText:{
+  formNumText: {
     fontSize: 22,
     fontWeight: 700
   },
@@ -155,5 +190,5 @@ const styles = StyleSheet.create({
     width: 28,
     marginBottom: 10
   },
-  
+
 })
